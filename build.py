48a0016010919e57981dba6ac138be23e73a1603
Mihai Damian <damianmihairadu@gmail.com>
2011-06-10 14:47:35 +0300

    Modified build script to work with subfolders

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
-from glob import iglob
 import shutil
+import fnmatch
 import os
 
 
@@ -13,7 +13,12 @@ if not os.path.exists(BUILD_DIR):
     os.makedirs(BUILD_DIR)
 
 output = open(os.path.join(BUILD_DIR, BUILD_PRODUCT_NAME), 'wb')
-for module in iglob(os.path.join(SRC_DIR, '*.js')):
-    shutil.copyfileobj(open(module, 'rb'), output)
-    output.write('\n\n'.encode('UTF-8'))
+print('Concatenating files...')
+for root, dirs, filenames in os.walk(SRC_DIR):
+    for filename in fnmatch.filter(filenames, '*.js'):
+        filepath = os.path.join(root, filename)
+        print(filepath)
+        shutil.copyfileobj(open(filepath, 'rb'), output)
 output.close()
+
+print('Done.')
